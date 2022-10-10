131c88d280e2b8949fbc2940783d6707d8c39d50
NiushaH <35556570+NiushaH@users.noreply.github.com>
2022-10-10 09:48:12 -0600

    fetch and display toy collection using CSS tags

--- src/index.js
+++ src/index.js
@@ -17,18 +17,37 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+
 function fetchToys() {
   fetch('http://localhost:3000/toys')
   .then(resp => resp.json())
-  // .then(json => console.log(json))
   .then(json => renderToys(json))
-}
+};
+
+// BEFORE RENDERING CSS TO PAGE
+// function renderToys(toys) {
+//   const main = document.getElementById('toy-collection');
+//   toys.forEach(toy => {
+//     const div = document.createElement('div');
+//     div.setAttribute("class", "card");
+//     div.innerHTML += `<p>Toy Name ${toy.name}</p>
+//                       <img src=${toy.image} >
+//                       <p> Likes ${toy.likes}</p>`;
+//     main.appendChild(div)
+//   })
+// }
 
 function renderToys(toys) {
   const main = document.getElementById('toy-collection');
   toys.forEach(toy => {
-    const div = document.createElement('div')
-    div.innerHTML = toy.image, toy.name, toy.likes
+    const div = document.createElement('div');
+    div.setAttribute("class", "card");
+    div.innerHTML += `<h2>${toy.name}</h2>
+                      <img src="${toy.image}" class="toy-avatar" />
+                      <p>${toy.likes} Likes </p>
+                      <button class="like-btn">Like <3</button>`;
     main.appendChild(div)
-  })
-}
+  });
+};
+
+fetchToys()
